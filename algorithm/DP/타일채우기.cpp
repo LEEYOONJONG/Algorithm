c6c6f6a8d0dcb62748b511fb21f12d67fbbadeeb
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-04-29 23:45:00 +0900

    210429

---
+++ algorithm/DP/타일채우기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if (n % 2 == 1) {
+		cout << 0;
+		return 0;
+	}
+
+}
--- algorithm/baekjoon/상담.cpp
+++ algorithm/baekjoon/상담.cpp
@@ -6,31 +6,43 @@
 #include <queue>
 using namespace std;
 
-int arrT[6], arrP[6];
+int arrT[16], arrP[16];
 int n;
 
 int outing(int start) {
-	if (start >= n) return 0;
+	cout << start << endl;
 
+	if (start+arrT[start] > n) return 0;
+
+	int maximum = arrP[start];
 	int sum = arrP[start];
-	for (int i = start + arrT[start]; i < n; i++) {
+
+	for (int i = start+1; i < n; i++) {
+		if (i + arrT[i] >= n) continue;
 		sum += outing(i);
+		if (sum > maximum) maximum = sum;
+		sum = arrP[start];
 	}
+
+	return maximum;
 
 }
 
 int main()
 {
-	
+
 	cin >> n;
-	
+
 	for (int i = 0; i < n; i++) {
 		cin >> arrT[i] >> arrP[i];
 	}
-
-	int max = -1;
+	
+	int maxi = -1;
 	for (int i = 0; i < n; i++) {
 		int tmp = outing(i);
-		if (tmp > max) max = tmp;
+		if (tmp > maxi) maxi = tmp;
 	}
+
+	cout << maxi;
+}
 	
