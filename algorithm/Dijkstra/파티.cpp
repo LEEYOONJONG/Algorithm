41d9cdef8760f999d73bbb3eb34bbc981afa5f36
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-05-26 22:02:19 +0900

    210526

---
+++ algorithm/Dijkstra/파티.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+int main() {
+	int n, m, x;
+	cin >> n >> m >> x;
+
+}
